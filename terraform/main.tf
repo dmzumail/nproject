9ab4eb108a26b5e712abadeb4d372e6bd890529f
dmzumail <dmzumail@gmail.com>
2025-11-11 18:35:15 +0300

    Доработаны файлы для terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,3 @@
-# Настройка провайдера Yandex Cloud
-terraform {
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = ">= 0.90"
-    }
-  }
-  required_version = ">= 1.0"
-}
-
-provider "yandex" {
-  token     = var.yc_token
-  cloud_id  = var.yc_cloud_id
-  folder_id = var.yc_folder_id
-  zone      = "ru-central1-a"
-}
-
 # Создаём публичную DNS-зону для твоего домена
 resource "yandex_dns_zone" "public" {
   name        = "zone-${var.domain}"
