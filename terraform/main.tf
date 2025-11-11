089fe401e6e41fa28a7e9f37f53df903a77f85a6
dmzumail <dmzumail@gmail.com>
2025-11-11 14:35:42 +0300

    Добавлен шаблон для terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+# Настройка провайдера Yandex Cloud
 terraform {
   required_providers {
     yandex = {
@@ -15,10 +16,21 @@ provider "yandex" {
   zone      = "ru-central1-a"
 }
 
-# Создаём DNS-зону для домена
-resource "yandex_dns_zone" "main" {
+# Создаём публичную DNS-зону для твоего домена
+resource "yandex_dns_zone" "public" {
   name        = "zone-${var.domain}"
-  description = "Managed zone for ${var.domain}"
-  zone       = "${var.domain}."
+  description = "Public DNS zone for ${var.domain}"
+  zone        = "${var.domain}."
   public      = true
 }
+
+# Пример: A-запись на внешний IP (опционально, раскомментируй позже)
+/*
+resource "yandex_dns_recordset" "www" {
+  zone_id = yandex_dns_zone.public.id
+  name    = "www.${var.domain}."
+  type    = "A"
+  ttl     = 300
+  data    = [var.public_ip]
+}
+*/
