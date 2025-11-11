832a6adf30d44f73a3e27418b62d8bc49c2de086
dmzumail <dmzumail@gmail.com>
2025-11-11 23:41:31 +0300

    Изменен файл провадера

---
+++ terraform/providers.tf
@@ -0,0 +1,17 @@
+# Настройка провайдера Yandex Cloud
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "~> 0.95"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
+provider "yandex" {
+  token     = var.yc_token
+  cloud_id  = var.yc_cloud_id
+  folder_id = var.yc_folder_id
+  zone      = "ru-central1-a"
+}
