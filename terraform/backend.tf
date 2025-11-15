cd85133b2b51b14b4f40d9e981555f320c12b8c4
dmzumail <dmzumail@gmail.com>
2025-11-15 16:12:47 +0300

    chore: обновление terraform.yml файла

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "s3" {
     bucket = "tfstate-nproject-site"
-
     key    = "terraform.tfstate"
     region = "ru-central1"
 
@@ -13,7 +12,7 @@ terraform {
     skip_metadata_api_check     = true
     skip_region_validation      = true
     skip_requesting_account_id  = true
-
-    use_lockfile = true  # для защиты от конфликтов
+    force_path_style            = true
+    use_lockfile                = true
   }
 }
